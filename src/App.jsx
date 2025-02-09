315d9a21c567e0062ac3ff9c31c1b2c9764fb4a9
Stan Cosmin <stancosminioan@gmail.com>
2025-02-09 17:39:30 +0200

    second commit -- personal website starter

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import stancLogo from "../public/stanc-logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={stancLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Stanc.dev</h1>
+    </>
+  );
+}
+
+export default App;
